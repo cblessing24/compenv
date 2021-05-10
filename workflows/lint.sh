fe671ae3a6e065ca3f507f1797a953a344bdecb2
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-05-10 13:05:15 +0200

    Add workflow for linting code

---
+++ workflows/lint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+linters=('black --check --diff .' 'isort --check --diff .' 'flake8 .' 'mypy .' 'pylint repro')
+
+echo 'Building Docker image...'
+docker build -t reproducibility . || exit 1
+
+exit_code=0
+for linter in "${linters[@]}"
+do
+    echo "Checking code with '$linter'..."
+    if ! eval "docker run reproducibility $linter";
+    then
+        exit_code=1
+    fi
+done
+exit "$exit_code"
