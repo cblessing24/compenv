a17cc9befd1e198032664feac15183be541ec9c9
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-05-11 14:33:25 +0200

    Rewrite hook decorator

--- repro/hook.py
+++ repro/hook.py
@@ -4,26 +4,27 @@ from typing import Callable, Mapping, Type, TypeVar, Union
 from datajoint.table import Table
 
 Key = Mapping[str, Union[int, str]]
-MakeMethod = Callable[[Table, Key], None]
-
-
 TableTypeVar = TypeVar("TableTypeVar", bound=Table)
 
 
-def add_hooks_to_make_method(
-    pre_hook: MakeMethod, post_hook: MakeMethod
+def hook_into_make_method(
+    hook: Callable[[Callable[[Key], None], Table, Key], None]
 ) -> Callable[[Type[TableTypeVar]], Type[TableTypeVar]]:
-    """Add a hook before and after the decorated table's make method."""
+    """Give control over the execution of a table's make method to a callable hook.
 
-    def _add_hooks_to_make_method(table_cls: Type[TableTypeVar]) -> Type[TableTypeVar]:
+    This function is meant to be used as a decorator. It will modify the make method of the decorated table. Calling
+    the modified make method will call the hook that was passed into the decorator with a reference to the original make
+    method, the table instance and the key. The original make method can the be executed by calling it with the table
+    instance and the key. This allows the execution of arbitrary code before and after the original make method.
+    """
+
+    def _hook_into_make_method(table_cls: Type[TableTypeVar]) -> Type[TableTypeVar]:
         original_make_method = table_cls.make
 
-        def make_method_with_hooks(table: TableTypeVar, key: Key) -> None:
-            pre_hook(table, key)
-            original_make_method(table, key)
-            post_hook(table, key)
+        def hooked_make_method(table: TableTypeVar, key: Key) -> None:
+            hook(original_make_method, table, key)
 
-        table_cls.make = make_method_with_hooks
+        table_cls.make = hooked_make_method
         return table_cls
 
-    return _add_hooks_to_make_method
+    return _hook_into_make_method
--- tests/unit/test_hook.py
+++ tests/unit/test_hook.py
@@ -1,84 +1,36 @@
 import pytest
 
-from repro.hook import add_hooks_to_make_method
+from repro.hook import hook_into_make_method
 
 
 @pytest.fixture
-def hook_cls():
+def hook():
     class Hook:
-        def __init__(self, name):
-            self.name = name
-            self.call = None
-            self.raise_error = False
-
-        def __call__(self, table, key):
-            if self.raise_error:
-                raise RuntimeError
-            self.call = (table, key)
-
-        def __repr__(self):
-            return self.name
-
-    return Hook
-
-
-@pytest.fixture
-def pre_hook(hook_cls):
-    return hook_cls("pre_hook")
-
-
-@pytest.fixture
-def post_hook(hook_cls):
-    return hook_cls("post_hook")
-
-
-@pytest.fixture
-def hooked_table(pre_hook, post_hook):
-    class Table:
         def __init__(self):
-            self.make_was_called = False
-            self.raise_error = False
+            self.args = None
 
-        def make(self, key):
-            if self.raise_error:
-                raise RuntimeError
-            self.make_was_called = True
+        def __call__(self, *args):
+            self.args = args
 
-    hooked_table = add_hooks_to_make_method(pre_hook, post_hook)(Table)
-    return hooked_table()
+    return Hook()
 
 
-def test_if_call_to_pre_hook_is_correct(hooked_table, pre_hook):
-    hooked_table.make("key")
-    assert pre_hook.call == (hooked_table, "key")
+@pytest.fixture
+def original_make_method():
+    def make(self, key):
+        pass
+
+    return make
 
 
-def test_if_make_method_gets_called(hooked_table):
-    hooked_table.make("key")
-    assert hooked_table.make_was_called
+@pytest.fixture
+def table(hook, original_make_method):
+
+    Table = type("Table", tuple(), {"make": original_make_method})
+    table = hook_into_make_method(hook)(Table)
+    return table()
 
 
-def test_if_call_to_post_hook_is_correct(hooked_table, post_hook):
-    hooked_table.make("key")
-    assert post_hook.call == (hooked_table, "key")
-
-
-def test_if_pre_hook_gets_called_before_post_hook(hooked_table, pre_hook, post_hook):
-    pre_hook.raise_error = True
-    with pytest.raises(RuntimeError):
-        hooked_table.make("key")
-    assert not post_hook.call
-
-
-def test_if_pre_hook_gets_called_before_make_method(hooked_table, pre_hook):
-    pre_hook.raise_error = True
-    with pytest.raises(RuntimeError):
-        hooked_table.make("key")
-    assert not hooked_table.make_was_called
-
-
-def test_if_make_method_gets_called_before_post_hook(hooked_table, post_hook):
-    hooked_table.raise_error = True
-    with pytest.raises(RuntimeError):
-        hooked_table.make("key")
-    assert not post_hook.call
+def test_if_hook_gets_called_with_correct_arguments(table, hook, original_make_method):
+    table.make("key")
+    assert hook.args == (original_make_method, table, "key")
