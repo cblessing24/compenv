30c5ec6e9b282472be85316b1e861614436a9cd9
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-05-28 12:59:57 +0200

    Improve distribution converter
    * Filter out non-python files
    * Filter out non-existing files
    * Convert valid files to modules

--- repro/distribution.py
+++ repro/distribution.py
@@ -1,8 +1,9 @@
 """Contains code related to getting information about installed distributions."""
 from functools import cache
 from importlib import metadata
+from pathlib import PurePath
 
-from .model import Distribution
+from .model import Distribution, Module
 
 
 class InstalledDistributionConverter:
@@ -15,10 +16,23 @@ class InstalledDistributionConverter:
         """Return a dictionary containing all installed distributions."""
         conv_dists: dict[str, Distribution] = {}
         for orig_dist in self._get_installed_distributions_func():
-            conv_files = set(str(f.locate()) for f in orig_dist.files) if orig_dist.files else set()
-            conv_dist = Distribution(orig_dist.metadata["Name"], orig_dist.metadata["Version"], conv_files)
+            conv_dist = self._convert_distribution(orig_dist)
             conv_dists[conv_dist.name] = conv_dist
         return conv_dists
+
+    def _convert_distribution(self, orig_dist: metadata.Distribution) -> Distribution:
+        if orig_dist.files:
+            modules = self._convert_files_to_modules(set(orig_dist.files))
+        else:
+            modules = set()
+        return Distribution(orig_dist.metadata["Name"], orig_dist.metadata["Version"], modules)
+
+    @staticmethod
+    def _convert_files_to_modules(files: set[metadata.PackagePath]) -> set[Module]:
+        valid_files = {f for f in files if f.suffix == ".py"}
+        abs_files = {f.locate() for f in valid_files}
+        existing_files = {f for f in abs_files if f.exists()}  # type: ignore
+        return {Module(f.stem, str(f)) for f in existing_files}  # type: ignore
 
     def __repr__(self) -> str:
         """Return a string representation of the translator."""
--- tests/unit/test_distribution.py
+++ tests/unit/test_distribution.py
@@ -1,15 +1,17 @@
 import pytest
 
 from repro.distribution import InstalledDistributionConverter
-from repro.model import Distribution
+from repro.model import Distribution, Module
 
 
 class FakePath:
-    def __init__(self, path):
+    def __init__(self, path, exists=True):
         self.path = path
+        self.suffix = "." + self.path.split(".")[-1]
+        self._exists = exists
 
     def locate(self):
-        return FakeAbsolutePath(self.path)
+        return FakeAbsolutePath(self.path, exists=self._exists)
 
     def __str__(self):
         raise RuntimeError("Path should be made absolute before converting to string")
@@ -18,6 +20,14 @@ class FakePath:
 class FakeAbsolutePath(FakePath):
     def __str__(self):
         return self.path
+
+    def exists(self):
+        return self._exists
+
+    @property
+    def stem(self):
+        name = self.path.split("/")[-1]
+        return name.split(".")[0]
 
 
 class FakeImportlibMetadataDistribution:
@@ -28,12 +38,29 @@ class FakeImportlibMetadataDistribution:
 
 @pytest.fixture
 def fake_distribution_metadata():
-    return {"foo": {"Name": "foo", "Version": "0.1.0"}, "bar": {"Name": "bar", "Version": "0.1.2"}}
+    return {
+        "foo": {"Name": "foo", "Version": "0.1.0"},
+        "bar": {"Name": "bar", "Version": "0.1.2"},
+        "baz": {"Name": "baz", "Version": "1.2.3"},
+    }
 
 
 @pytest.fixture
 def fake_distribution_files():
-    return {"foo": [FakePath("/foo/module1.py"), FakePath("/foo/module2.py")], "bar": [FakePath("/bar/module.py")]}
+    return {
+        "foo": [
+            FakePath("/foo/module1.py"),
+            FakePath("/foo/README.md"),
+            FakePath("/foo/module2.py"),
+            FakePath("/foo/tests/module3.py", exists=False),
+        ],
+        "bar": [
+            FakePath("/bar/tests/module5.py", exists=False),
+            FakePath("/bar/requirements.txt"),
+            FakePath("/bar/module.py"),
+        ],
+        "baz": None,
+    }
 
 
 @pytest.fixture
@@ -61,8 +88,11 @@ def converter(fake_get_installed_distributions_func):
 
 def test_correct_distributions_returned(converter):
     expected_distributions = {
-        "foo": Distribution("foo", "0.1.0", {"/foo/module1.py", "/foo/module2.py"}),
-        "bar": Distribution("bar", "0.1.2", {"/bar/module.py"}),
+        "foo": Distribution(
+            "foo", "0.1.0", {Module("module1", "/foo/module1.py"), Module("module2", "/foo/module2.py")}
+        ),
+        "bar": Distribution("bar", "0.1.2", {Module("module", "/bar/module.py")}),
+        "baz": Distribution("baz", "1.2.3", set()),
     }
     actual_distributions = converter()
     assert actual_distributions == expected_distributions
