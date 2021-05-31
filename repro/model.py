32d821ae688794e66254c85bcaf9630b178c49cd
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-05-31 17:00:00 +0200

    Add package and modular unit classes

--- repro/model.py
+++ repro/model.py
@@ -1,16 +1,45 @@
 """Contains the domain model."""
 from __future__ import annotations
 
+from abc import ABC
 from dataclasses import dataclass, field
-from typing import Collection, Iterator, Optional
+from typing import Collection, Container, Iterator, Optional
 
 
 @dataclass(frozen=True)
-class Module:
-    """An imported module."""
+class ModularUnit(ABC, Container["ModularUnit"]):
+    """Represents a modular unit used to organize Python code."""
 
     name: str
-    file: Optional[str] = None
+    file: Optional[str]
+
+    def __contains__(self, unit: object) -> bool:
+        """Return true if the unit is part of this unit or any sub-units."""
+        if not isinstance(unit, ModularUnit):
+            raise TypeError
+        return unit == self
+
+
+@dataclass(frozen=True)
+class Module(ModularUnit):
+    """Represents a Python module."""
+
+
+@dataclass(frozen=True, init=False)
+class Package(ModularUnit):
+    """Represents a Python package."""
+
+    _units: frozenset[ModularUnit] = field(repr=False)
+
+    def __init__(self, name, file, units):
+        """Initialize distribution."""
+        object.__setattr__(self, "name", name)
+        object.__setattr__(self, "file", file)
+        object.__setattr__(self, "_units", units)
+
+    def __contains__(self, unit: object) -> bool:
+        """Return true if the unit is part of this unit or any sub-units."""
+        return super().__contains__(unit) or any(unit in package_unit for package_unit in self._units)
 
 
 @dataclass(frozen=True, init=False)
--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -1,30 +1,65 @@
 import pytest
 
-from repro.model import Distribution, Module
+from repro.model import Distribution, ModularUnit, Module, Package
 
 
-@pytest.fixture
-def distribution():
-    return Distribution("bar", "0.1.0", {Module("module", "/foo/module.py")})
+class TestModularUnit:
+    @staticmethod
+    def test_contains_raises_error_when_called_with_non_modular_unit_object():
+        with pytest.raises(TypeError):
+            object in ModularUnit("module", file=None)
+
+    @staticmethod
+    def test_modular_unit_contains_itself():
+        module = ModularUnit("module", file=False)
+        assert module in module
 
 
-def test_non_module_object_not_in_distribution(distribution):
-    assert object not in distribution
+class TestPackage:
+    @staticmethod
+    def test_modular_unit_in_package_if_in_package_itself():
+        unit = ModularUnit("module", file=None)
+        package = Package("package", file=None, units={unit})
+        assert unit in package
+
+    @staticmethod
+    def test_modular_unit_in_package_if_in_sub_package():
+        modular_unit = ModularUnit("module", file=None)
+        sub_package = Package("sub-package", file=None, units=frozenset([modular_unit]))
+        package = Package("package", file=None, units=frozenset([sub_package]))
+        assert modular_unit in package
+
+    @staticmethod
+    def test_module_not_in_package_if_not_in_package_or_sub_packages():
+        module = Module("module", file=None)
+        package = Package("package", file=None, units=frozenset())
+        assert module not in package
 
 
-def test_module_in_distribution_if_module_in_distribution_modules(distribution):
-    module = Module("module", "/foo/module.py")
-    assert module in distribution
+class TestDistribution:
+    @staticmethod
+    @pytest.fixture
+    def distribution():
+        return Distribution("bar", "0.1.0", {Module("module", "/foo/module.py")})
 
+    @staticmethod
+    def test_non_module_object_not_in_distribution(distribution):
+        assert object() not in distribution
 
-def test_module_not_in_distribution_if_module_not_in_distribution_modules(distribution):
-    module = Module("foo", "/bar/module.py")
-    assert module not in distribution
+    @staticmethod
+    def test_module_in_distribution_if_module_in_distribution_modules(distribution):
+        module = Module("module", "/foo/module.py")
+        assert module in distribution
 
+    @staticmethod
+    def test_module_not_in_distribution_if_module_not_in_distribution_modules(distribution):
+        module = Module("foo", "/bar/module.py")
+        assert module not in distribution
 
-def test_length_equal_to_number_of_modules_in_distribution(distribution):
-    assert len(distribution) == 1
+    @staticmethod
+    def test_length_equal_to_number_of_modules_in_distribution(distribution):
+        assert len(distribution) == 1
 
-
-def test_iterating_distribution_iterates_modules(distribution):
-    assert next(iter(distribution)) == Module("module", "/foo/module.py")
+    @staticmethod
+    def test_iterating_distribution_iterates_modules(distribution):
+        assert next(iter(distribution)) == Module("module", "/foo/module.py")
