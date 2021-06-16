5657216c7e59f8a782a3c9e15842343e9aad66dc
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-06-16 16:07:46 +0200

    Initialize domain model

--- repro/__init__.py
+++ repro/__init__.py
@@ -1,1 +1,7 @@
 """Contains reproducibility tools."""
+from . import model
+from .distribution import InstalledDistributionConverter
+from .module import LoadedModuleConverter
+
+model.get_installed_distributions = InstalledDistributionConverter()
+model.get_loaded_modules = LoadedModuleConverter()
