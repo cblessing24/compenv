c770e6f4568297dea091fa0fe1e2f84725f990bc
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-08-02 14:47:30 +0200

    Separate environment code into environment module

--- tests/unit/model/conftest.py
+++ tests/unit/model/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 
+from repro.model import record as record_module
+from repro.model.environment import Environment
 from repro.model.record import Distribution, Module, Record
 
 
@@ -30,3 +32,16 @@ def record(installed_distributions, active_modules, active_distributions):
         active_distributions=active_distributions,
         active_modules=active_modules,
     )
+
+
+@pytest.fixture
+def environment(installed_distributions, active_modules):
+    def fake_get_active_modules():
+        return iter(active_modules)
+
+    def fake_get_installed_distributions():
+        return iter(installed_distributions)
+
+    record_module.get_active_modules = fake_get_active_modules
+    record_module.get_installed_distributions = fake_get_installed_distributions
+    return Environment()
--- tests/unit/model/test_model.py
+++ tests/unit/model/test_model.py
@@ -17,19 +17,6 @@ class TestComputationRecord:
             setattr(computation_record, attr, "another_value")
 
 
-@pytest.fixture
-def environment(installed_distributions, active_modules):
-    def fake_get_active_modules():
-        return iter(active_modules)
-
-    def fake_get_installed_distributions():
-        return iter(installed_distributions)
-
-    record.get_active_modules = fake_get_active_modules
-    record.get_installed_distributions = fake_get_installed_distributions
-    return Environment()
-
-
 class TestComputation:
     @staticmethod
     @pytest.fixture
@@ -114,25 +101,3 @@ class TestComputation:
             repr(computation)
             == f"Computation(identifier='identifier', environment=Environment(), trigger=FakeTrigger())"
         )
-
-
-class TestEnvironment:
-    @staticmethod
-    def test_correct_record_is_recorded(environment, record):
-        assert environment.record() == record
-
-    @staticmethod
-    @pytest.mark.parametrize("attr", ["success", "record"])
-    def test_consistency_check_attributes_can_not_be_accessed_in_with_block(environment, attr):
-        with pytest.raises(RuntimeError, match=f"Can not access '{attr}'"):
-            with environment.consistency_check() as check:
-                getattr(check, attr)
-
-    @staticmethod
-    def test_consistency_check_repr(environment):
-        with environment.consistency_check() as check:
-            assert repr(check) == "_ConsistencyCheck(environment=Environment())"
-
-    @staticmethod
-    def test_repr(environment):
-        assert repr(environment) == "Environment()"
