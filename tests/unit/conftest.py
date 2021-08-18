dd46d99a5ef48f102dbd1fbdb89ace4b8a20ac56
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-08-18 14:11:04 +0200

    Move fixtures up

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pytest
 
+from repro.adapters.repository import DJComputationRecord, DJDistribution, DJModule, DJModuleAffiliation
 from repro.model.record import (
     ActiveDistributions,
     ActiveModules,
@@ -39,4 +40,46 @@ def record(installed_distributions, active_modules):
     return Record(
         installed_distributions=installed_distributions,
         active_modules=active_modules,
+    )
+
+
+@pytest.fixture
+def primary():
+    return {"a": 0, "b": 1}
+
+
+@pytest.fixture
+def dj_modules():
+    return frozenset(
+        [
+            DJModule(module_file="module1.py", module_is_active="False"),
+            DJModule(module_file="module2.py", module_is_active="True"),
+        ]
+    )
+
+
+@pytest.fixture
+def dj_dists():
+    return frozenset(
+        [
+            DJDistribution(distribution_name="dist1", distribution_version="0.1.0"),
+            DJDistribution(distribution_name="dist2", distribution_version="0.1.1"),
+        ]
+    )
+
+
+@pytest.fixture
+def dj_module_affiliations():
+    return frozenset(
+        [
+            DJModuleAffiliation(module_file="module1.py", distribution_name="dist1"),
+            DJModuleAffiliation(module_file="module2.py", distribution_name="dist2"),
+        ]
+    )
+
+
+@pytest.fixture
+def dj_comp_rec(primary, dj_modules, dj_dists, dj_module_affiliations):
+    return DJComputationRecord(
+        primary=primary, modules=dj_modules, distributions=dj_dists, module_affiliations=dj_module_affiliations
     )
