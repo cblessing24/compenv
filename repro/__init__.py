dbc2e0662b82445cd27b4baacf4ee146f5c5d0e4
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-08-24 13:10:19 +0200

    Reimport record_environment on top level

--- repro/__init__.py
+++ repro/__init__.py
@@ -1,6 +1,7 @@
 """Contains reproducibility tools."""
 from .adapters.distribution import InstalledDistributionConverter
 from .adapters.module import ActiveModuleConverter
+from .infrastructure.entrypoint import record_environment
 from .model import record
 
 record.get_installed_distributions = InstalledDistributionConverter()
