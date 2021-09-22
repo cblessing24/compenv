64b4cb54f9c3d7f4dddd93a577954da6d51873a8
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-09-22 12:45:10 +0200

    Fix type annotations

--- repro/infrastructure/hook.py
+++ repro/infrastructure/hook.py
@@ -1,16 +1,16 @@
 """Contains code related to hooks."""
 from typing import Callable, Type, TypeVar
 
-from datajoint.table import Table
+from datajoint.autopopulate import AutoPopulate
 
 from ..adapters.translator import PrimaryKey
 
-TableTypeVar = TypeVar("TableTypeVar", bound=Table)
+_T = TypeVar("_T", bound=AutoPopulate)
 
 
 def hook_into_make_method(
-    hook: Callable[[Callable[[PrimaryKey], None], Table, PrimaryKey], None]
-) -> Callable[[Type[TableTypeVar]], Type[TableTypeVar]]:
+    hook: Callable[[Callable[[PrimaryKey], None], _T, PrimaryKey], None]
+) -> Callable[[Type[_T]], Type[_T]]:
     """Give control over the execution of a table's make method to a callable hook.
 
     This function is meant to be used as a decorator. It will modify the make method of the decorated table. Calling
@@ -19,10 +19,10 @@ def hook_into_make_method(
     instance and the key. This allows the execution of arbitrary code before and after the original make method.
     """
 
-    def _hook_into_make_method(table_cls: Type[TableTypeVar]) -> Type[TableTypeVar]:
+    def _hook_into_make_method(table_cls: Type[_T]) -> Type[_T]:
         original_make_method = table_cls.make
 
-        def hooked_make_method(table: TableTypeVar, key: PrimaryKey) -> None:
+        def hooked_make_method(table: _T, key: PrimaryKey) -> None:
             hook(original_make_method, table, key)
 
         table_cls.make = hooked_make_method
