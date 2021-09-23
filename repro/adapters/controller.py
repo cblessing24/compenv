d0b5329ca78c160c9fb7738bb35e85766a0a05e8
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-09-23 16:33:35 +0200

    Implement controller

---
+++ repro/adapters/controller.py
@@ -0,0 +1,33 @@
+"""Contains the controller."""
+import functools
+from typing import Callable
+
+from ..service.record import RecordService
+from .presenter import DJPresenter
+from .repository import DJCompRecRepo
+from .translator import DJTranslator, PrimaryKey
+
+
+class DJController:
+    """Controls the execution of services."""
+
+    def __init__(self, repo: DJCompRecRepo, translator: DJTranslator, presenter: DJPresenter) -> None:
+        """Initialize the controller."""
+        self.repo = repo
+        self.translator = translator
+        self.presenter = presenter
+
+    def record(self, key: PrimaryKey, make: Callable[[PrimaryKey], None]) -> None:
+        """Execute the record service."""
+        ident = self.translator.to_identifier(key)
+        service = RecordService(self.repo, output_port=self.presenter.record)
+        trigger = functools.partial(make, key)
+        request = service.create_request(ident, trigger=trigger)
+        service(request)
+
+    def __repr__(self) -> str:
+        """Return a string representation of the controller."""
+        return (
+            f"{self.__class__.__name__}(repo={repr(self.repo)},"
+            f" translator={repr(self.translator)}, presenter={repr(self.presenter)})"
+        )
