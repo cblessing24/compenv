16636b92b1c098196f7f463c2c7fc6ec52d26e5b
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-09-24 14:52:12 +0200

    Move fake facade fixture to conftest

--- tests/unit/adapters/test_repository.py
+++ tests/unit/adapters/test_repository.py
@@ -1,43 +1,8 @@
 import pytest
 
 from repro.adapters.entity import DJComputationRecord, DJModule
-from repro.adapters.repository import AbstractTableFacade, DJCompRecRepo
+from repro.adapters.repository import DJCompRecRepo
 from repro.model.computation import ComputationRecord
-
-
-@pytest.fixture
-def fake_facade():
-    class FakeRecordTableFacade(AbstractTableFacade[DJComputationRecord]):
-        def __init__(self):
-            self.dj_comp_recs = []
-
-        def __setitem__(self, primary, entity):
-            if (primary, entity) in self.dj_comp_recs:
-                raise ValueError
-            self.dj_comp_recs.append((primary, entity))
-
-        def __delitem__(self, primary):
-            try:
-                del self.dj_comp_recs[next(i for i, (p, _) in enumerate(self.dj_comp_recs) if p == primary)]
-            except StopIteration as error:
-                raise KeyError from error
-
-        def __getitem__(self, primary):
-            try:
-                return next(r for (p, r) in self.dj_comp_recs if p == primary)
-            except StopIteration as error:
-                raise KeyError from error
-
-        def __iter__(self):
-            return (p for (p, _) in self.dj_comp_recs)
-
-        def __len__(self):
-            return len(self.dj_comp_recs)
-
-        def __repr__(self):
-            return self.__class__.__name__ + "()"
-
-    return FakeRecordTableFacade()
 
 
 @pytest.fixture
