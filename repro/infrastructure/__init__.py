f2e80696167806d755e016b435a487fe610a2417
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-09-24 15:38:34 +0200

    Add function for creating infrastructue layer

--- repro/infrastructure/__init__.py
+++ repro/infrastructure/__init__.py
@@ -1,1 +1,22 @@
 """This package contains the infrastructure layer."""
+import dataclasses
+
+from datajoint import Schema
+
+from .facade import RecordTableFacade
+from .factory import RecordTableFactory
+
+
+@dataclasses.dataclass(frozen=True)
+class DJInfrastructure:
+    """A set of DataJoint infrastructure objects."""
+
+    factory: RecordTableFactory
+    facade: RecordTableFacade
+
+
+def create_dj_infrastructure(schema: Schema, table_name: str) -> DJInfrastructure:
+    """Create a set of DataJoint infrastructure objects."""
+    factory = RecordTableFactory(schema, parent=table_name)
+    facade = RecordTableFacade(factory=factory)
+    return DJInfrastructure(factory=factory, facade=facade)
