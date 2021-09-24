04aa938ad3268b7aa94477df781faa6b4a9b1d2c
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-09-24 14:53:13 +0200

    Add function that creates dj adapters

--- repro/adapters/__init__.py
+++ repro/adapters/__init__.py
@@ -1,1 +1,29 @@
 """This package contains adapters adapting between external systems and the domain/service layers."""
+import dataclasses
+
+from repro.adapters.controller import DJController
+from repro.adapters.entity import DJComputationRecord
+from repro.adapters.presenter import DJPresenter
+from repro.adapters.repository import DJCompRecRepo
+from repro.adapters.translator import DJTranslator, blake2b
+
+from .abstract import AbstractTableFacade
+
+
+@dataclasses.dataclass(frozen=True)
+class DJAdapters:
+    """A set of DataJoint adapters."""
+
+    translator: DJTranslator
+    controller: DJController
+    presenter: DJPresenter
+    repo: DJCompRecRepo
+
+
+def create_dj_adapters(facade: AbstractTableFacade[DJComputationRecord]) -> DJAdapters:
+    """Create a set of DataJoint adapters using the given facade."""
+    translator = DJTranslator(blake2b)
+    presenter = DJPresenter()
+    repo = DJCompRecRepo(facade=facade, translator=translator)
+    controller = DJController(repo=repo, translator=translator, presenter=presenter)
+    return DJAdapters(translator=translator, presenter=presenter, repo=repo, controller=controller)
---
+++ tests/unit/adapters/test_init.py
@@ -0,0 +1,63 @@
+from dataclasses import FrozenInstanceError, is_dataclass
+
+import pytest
+
+from repro.adapters import DJAdapters, create_dj_adapters
+from repro.adapters.controller import DJController
+from repro.adapters.presenter import DJPresenter
+from repro.adapters.repository import DJCompRecRepo
+from repro.adapters.translator import DJTranslator
+
+
+@pytest.fixture
+def dj_adapters(fake_facade):
+    return create_dj_adapters(fake_facade)
+
+
+def test_adapters_are_created(dj_adapters):
+    assert isinstance(dj_adapters, DJAdapters)
+
+
+def test_adapters_is_dataclass(dj_adapters):
+    assert is_dataclass(dj_adapters)
+
+
+def test_correct_translator_is_used(dj_adapters):
+    assert isinstance(dj_adapters.translator, DJTranslator)
+
+
+def test_correct_presenter_is_used(dj_adapters):
+    assert isinstance(dj_adapters.presenter, DJPresenter)
+
+
+def test_correct_repository_is_used(dj_adapters):
+    assert isinstance(dj_adapters.repo, DJCompRecRepo)
+
+
+def test_repo_uses_correct_translator(dj_adapters):
+    assert dj_adapters.translator is dj_adapters.repo.translator
+
+
+def test_repo_uses_correct_facade(fake_facade, dj_adapters):
+    assert dj_adapters.repo.facade is fake_facade
+
+
+def test_correct_controller_is_used(dj_adapters):
+    assert isinstance(dj_adapters.controller, DJController)
+
+
+def test_controller_uses_correct_translator(dj_adapters):
+    assert dj_adapters.controller.translator is dj_adapters.translator
+
+
+def test_controller_uses_correct_presenter(dj_adapters):
+    assert dj_adapters.controller.presenter is dj_adapters.presenter
+
+
+def test_controller_uses_correct_repository(dj_adapters):
+    assert dj_adapters.controller.repo is dj_adapters.repo
+
+
+def test_adapters_is_frozen(dj_adapters):
+    with pytest.raises(FrozenInstanceError):
+        dj_adapters.translator = "not a translator"
