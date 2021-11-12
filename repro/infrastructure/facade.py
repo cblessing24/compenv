d6d6b59863f5031636a8b7361ba47e31163a987d
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-11-12 17:12:05 +0100

    Import from proper locations in facade

--- repro/infrastructure/facade.py
+++ repro/infrastructure/facade.py
@@ -9,7 +9,8 @@ from datajoint.errors import DuplicateError
 
 from repro.infrastructure.factory import DJTableFactory
 
-from ..adapters.repository import AbstractTableFacade, DJComputationRecord
+from ..adapters.abstract import AbstractTableFacade
+from ..adapters.entity import DJComputationRecord
 from ..adapters.translator import PrimaryKey
 
 
