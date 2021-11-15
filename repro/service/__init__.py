1e173df20c44476b607eed284ed6f2543ee1e402
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-11-15 13:55:10 +0100

    Remove global SERVICES variable

--- repro/service/__init__.py
+++ repro/service/__init__.py
@@ -1,7 +1,1 @@
 """This package contains all use-cases."""
-from typing import Dict, Type
-
-from .abstract import Service
-from .record import RecordService
-
-SERVICES: Dict[str, Type[Service]] = {"record": RecordService}
