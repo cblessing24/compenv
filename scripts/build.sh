ed102b220522d997ec488058ac7ba8fa6f04fb23
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-04-04 14:24:32 +0200

    Add script for building Docker images

---
+++ scripts/build.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+build_base_image() {
+    ref=$(git rev-parse HEAD)
+    docker build --build-arg COMPENV_REF="$ref" -t compenv .
+}
+
+build_dev_image() {
+    ref=$(git ls-remote https://github.com/cblessing24/dotfiles main | cut -d '	' -f 1)
+    docker build --build-arg BASE=compenv --build-arg DOTFILES_REF="$ref" -f Dockerfile.dev -t compenv_dev .
+}
+
+case "$1" in
+    all)
+        build_base_image
+        build_dev_image
+        ;;
+    base)
+        build_base_image
+        ;;
+    dev)
+        build_dev_image
+        ;;
+    *)
+        echo "Usage: $0 {all|base|dev}"
+        exit 1
+esac
