81fac51522dfa23e362c4e184b1000aa66ee88e7
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-08-17 08:29:50 +0000

    Remove computation class

--- compenv/model/__init__.py
+++ compenv/model/__init__.py
@@ -1,3 +1,2 @@
 """This package contains the model layer."""
-from .computation import Computation  # noqa: F401
 from .environment import Environment  # noqa: F401
--- compenv/model/computation.py
+++ compenv/model/computation.py
@@ -2,50 +2,12 @@
 from __future__ import annotations
 
 import textwrap
-import warnings
 from dataclasses import dataclass
-from typing import Callable, NewType
+from typing import NewType
 
-from .environment import Environment
 from .record import Record
 
 Identifier = NewType("Identifier", str)
-
-
-class Computation:
-    """Represents a computation."""
-
-    def __init__(
-        self,
-        identifier: Identifier,
-        environment: Environment,
-        trigger: Callable[[], None],
-    ) -> None:
-        """Initialize the computation."""
-        self.identifier = identifier
-        self._environment = environment
-        self._trigger = trigger
-        self._is_executed = False
-
-    def execute(self) -> ComputationRecord:
-        """Execute the computation."""
-        if self._is_executed:
-            raise RuntimeError("Computation already executed!")
-        with self._environment.consistency_check() as check:
-            self._trigger()
-        if not check.success:
-            warnings.warn("Environment changed during execution!")
-        self._is_executed = True
-        return ComputationRecord(self.identifier, check.record)
-
-    def __repr__(self) -> str:
-        """Return a string representation of the computation."""
-        return (
-            f"{self.__class__.__name__}("
-            f"identifier={repr(self.identifier)}, "
-            f"environment={repr(self._environment)}, "
-            f"trigger={repr(self._trigger)})"
-        )
 
 
 @dataclass(frozen=True)
