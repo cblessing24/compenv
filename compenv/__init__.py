eef361b38bb82d058e7ac5d49f07a1657b112939
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-08-17 08:46:23 +0000

    Remove unnecessary initialization code

--- compenv/__init__.py
+++ compenv/__init__.py
@@ -1,8 +1,4 @@
 """Contains reproducibility tools."""
-from .adapters.distribution import DistributionConverter
 from .infrastructure.entrypoint import EnvironmentRecorder
-from .model import record
-
-record.get_distributions = DistributionConverter()
 
 record_environment = EnvironmentRecorder()
