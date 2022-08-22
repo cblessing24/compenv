c31e297acc77e01414df23f7668052a02141e1c5
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-08-22 12:39:10 +0000

    Make fake service generic

--- tests/unit/adapters/test_controller.py
+++ tests/unit/adapters/test_controller.py
@@ -1,10 +1,10 @@
-from typing import List, Optional, Type
+from typing import Generic, List, Optional, Type, TypeVar
 
 import pytest
 
 from compenv.adapters.controller import DJController
 from compenv.model.record import Identifier
-from compenv.service.abstract import Response
+from compenv.service.abstract import Request, Response
 from compenv.service.record import RecordRequest
 from compenv.types import PrimaryKey
 
@@ -27,32 +27,38 @@ def fake_presenter() -> FakePresenter:
     return FakePresenter()
 
 
-class FakeService:
-    request: RecordRequest
+T = TypeVar("T", bound=Request)
+
+
+class FakeService(Generic[T]):
+    request: T
+    request_cls: Type[T]
 
     @property
-    def create_request(self) -> Type[RecordRequest]:
-        return RecordRequest
+    def create_request(self) -> Type[T]:
+        return self.request_cls
 
-    def __call__(self, request: RecordRequest) -> None:
+    def __call__(self, request: T) -> None:
         self.request = request
 
 
 @pytest.fixture
-def fake_service() -> FakeService:
-    return FakeService()
+def fake_service() -> FakeService[RecordRequest]:
+    service: FakeService[RecordRequest] = FakeService()
+    service.request_cls = RecordRequest
+    return service
 
 
 @pytest.fixture
 def controller(
-    fake_service: FakeService,
+    fake_service: FakeService[RecordRequest],
     fake_translator: FakeTranslator,
 ) -> DJController:
     return DJController(fake_service, fake_translator)
 
 
 def test_record_request_has_appropriate_identifier(
-    controller: DJController, primary: PrimaryKey, fake_service: FakeService, identifier: Identifier
+    controller: DJController, primary: PrimaryKey, fake_service: FakeService[RecordRequest], identifier: Identifier
 ) -> None:
     controller.record(primary, lambda _: None)
     assert fake_service.request.identifier == identifier
@@ -67,7 +73,7 @@ class FakeMake:
 
 
 def test_record_request_has_appropriate_trigger(
-    controller: DJController, primary: PrimaryKey, fake_service: FakeService
+    controller: DJController, primary: PrimaryKey, fake_service: FakeService[RecordRequest]
 ) -> None:
     fake_make = FakeMake()
     controller.record(primary, fake_make)
