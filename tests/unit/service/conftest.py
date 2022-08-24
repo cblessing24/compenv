730ee2922aab00eac7f1fdc15a33d32dc9ecc2be
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-08-24 09:28:19 +0000

    Add service that tracks requests for testing

--- tests/unit/service/conftest.py
+++ tests/unit/service/conftest.py
@@ -1,7 +1,7 @@
 import dataclasses
 from typing import Callable, List
 
-from compenv.service.abstract import Request, Response, Service
+from compenv.service.abstract import _T, _V, Request, Response, Service
 
 
 @dataclasses.dataclass(frozen=True)
@@ -14,16 +14,21 @@ class FakeResponse(Response):
     message: str
 
 
-class FakeService(Service[FakeRequest, FakeResponse]):
+class RequestTrackingService(Service[_T, _V]):
+    def __init__(self, *, output_port: Callable[[_V], None]) -> None:
+        super().__init__(output_port=output_port)
+        self.requests: List[_T] = []
+
+    def __call__(self, request: _T) -> None:
+        self.requests.append(request)
+        super().__call__(request)
+
+
+class FakeService(RequestTrackingService[FakeRequest, FakeResponse]):
     name = "fake_service"
 
     _request_cls = FakeRequest
     _response_cls = FakeResponse
 
-    def __init__(self, *, output_port: Callable[[FakeResponse], None]) -> None:
-        super().__init__(output_port=output_port)
-        self.requests: List[FakeRequest] = []
-
     def _execute(self, request: FakeRequest) -> FakeResponse:
-        self.requests.append(request)
         return self._response_cls(request.message)
