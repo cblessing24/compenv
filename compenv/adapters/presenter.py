0e7fb0aaced408d17b358d1381a4e5d56a4e0f08
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-08-25 08:18:23 +0000

    Add diff method to presenter

--- compenv/adapters/presenter.py
+++ compenv/adapters/presenter.py
@@ -1,6 +1,8 @@
 """Contains the presenter."""
 from typing import Protocol
 
+from compenv.service.diff import DiffResponse
+
 from ..service.record import RecordResponse
 
 
@@ -16,6 +18,13 @@ class DJPresenter:
 
     def record(self, response: RecordResponse) -> None:
         """Present information contained within the record service's response."""
+
+    def diff(self, response: DiffResponse) -> None:
+        """Present information contained withing the diff service's response."""
+        if response.differ:
+            print("The computation records differ")
+        else:
+            print("The computation records do not differ")
 
     def __repr__(self) -> str:
         """Return a string representation of the presenter."""
--- tests/unit/adapters/test_presenter.py
+++ tests/unit/adapters/test_presenter.py
@@ -1,5 +1,25 @@
+import pytest
+from pytest import CaptureFixture
+
 from compenv.adapters.presenter import DJPresenter
+from compenv.service.diff import DiffResponse
 
 
 def test_repr() -> None:
     assert repr(DJPresenter()) == "DJPresenter()"
+
+
+@pytest.mark.parametrize(
+    "differ,expected",
+    [
+        (True, "The computation records differ\n"),
+        (False, "The computation records do not differ\n"),
+    ],
+)
+def test_information_in_diff_response_is_correctly_printed(
+    capsys: CaptureFixture[str], differ: bool, expected: str
+) -> None:
+    response = DiffResponse(differ=differ)
+    DJPresenter().diff(response)
+    captured = capsys.readouterr()
+    assert captured.out == expected
