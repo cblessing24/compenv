aec6fb7eac0f49133d49188356e91d1352036f37
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-09-14 13:47:52 +0000

    Remove superfluous computation record fixture

--- tests/unit/adapters/test_repository.py
+++ tests/unit/adapters/test_repository.py
@@ -4,7 +4,7 @@ import pytest
 
 from compenv.adapters.entity import DJComputationRecord
 from compenv.adapters.repository import DJRepository
-from compenv.model.record import ComputationRecord, Distribution, Identifier
+from compenv.model.record import ComputationRecord, Identifier
 from compenv.types import PrimaryKey
 
 from ..conftest import FakeTranslatorFactory
@@ -17,21 +17,16 @@ def repo(fake_translator_factory: FakeTranslatorFactory, fake_facade: FakeRecord
 
 
 @pytest.fixture
-def comp_rec(identifier: Identifier, distributions: frozenset[Distribution]) -> ComputationRecord:
-    return ComputationRecord(identifier, distributions)
-
-
-@pytest.fixture
-def add_computation_record(repo: DJRepository, comp_rec: ComputationRecord) -> None:
-    repo.add(comp_rec)
+def add_computation_record(repo: DJRepository, computation_record: ComputationRecord) -> None:
+    repo.add(computation_record)
 
 
 @pytest.mark.usefixtures("add_computation_record")
 class TestAdd:
     @staticmethod
-    def test_raises_error_if_already_existing(repo: DJRepository, comp_rec: ComputationRecord) -> None:
+    def test_raises_error_if_already_existing(repo: DJRepository, computation_record: ComputationRecord) -> None:
         with pytest.raises(ValueError, match="already exists!"):
-            repo.add(comp_rec)
+            repo.add(computation_record)
 
     @staticmethod
     def test_inserts_dj_computation_record(
@@ -49,18 +44,18 @@ class TestGet:
     @staticmethod
     @pytest.mark.usefixtures("add_computation_record")
     def test_gets_computation_record_if_existing(
-        repo: DJRepository, comp_rec: ComputationRecord, identifier: Identifier
+        repo: DJRepository, computation_record: ComputationRecord, identifier: Identifier
     ) -> None:
-        assert repo.get(identifier) == comp_rec
+        assert repo.get(identifier) == computation_record
 
 
-def test_iteration(repo: DJRepository, comp_rec: ComputationRecord) -> None:
-    repo.add(comp_rec)
-    assert list(iter(repo)) == [comp_rec.identifier]
+def test_iteration(repo: DJRepository, computation_record: ComputationRecord) -> None:
+    repo.add(computation_record)
+    assert list(iter(repo)) == [computation_record.identifier]
 
 
-def test_length(repo: DJRepository, comp_rec: ComputationRecord) -> None:
-    repo.add(comp_rec)
+def test_length(repo: DJRepository, computation_record: ComputationRecord) -> None:
+    repo.add(computation_record)
     assert len(repo) == 1
 
 
