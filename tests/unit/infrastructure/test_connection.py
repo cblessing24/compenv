076916ca31f74d162970ee44c06878991e70864c
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-09-22 09:17:32 +0000

    Put connection facade tests in class

--- tests/unit/infrastructure/test_connection.py
+++ tests/unit/infrastructure/test_connection.py
@@ -5,27 +5,29 @@ from compenv.infrastructure.connection import ConnectionFacade
 from ..conftest import FakeConnection
 
 
-@pytest.fixture
-def connection_facade(fake_connection: FakeConnection) -> ConnectionFacade:
-    return ConnectionFacade(fake_connection)
+class TestConnectionFacade:
+    @staticmethod
+    @pytest.fixture
+    def connection_facade(fake_connection: FakeConnection) -> ConnectionFacade:
+        return ConnectionFacade(fake_connection)
 
+    @staticmethod
+    def test_can_start_transaction(connection_facade: ConnectionFacade, fake_connection: FakeConnection) -> None:
+        connection_facade.start()
+        assert fake_connection.in_transaction
 
-def test_can_start_transaction(connection_facade: ConnectionFacade, fake_connection: FakeConnection) -> None:
-    connection_facade.start()
-    assert fake_connection.in_transaction
+    @staticmethod
+    def test_can_commit_transaction(connection_facade: ConnectionFacade, fake_connection: FakeConnection) -> None:
+        connection_facade.commit()
+        assert fake_connection.committed
 
+    @staticmethod
+    def test_can_rollback_transaction(connection_facade: ConnectionFacade, fake_connection: FakeConnection) -> None:
+        connection_facade.start()
+        connection_facade.rollback()
+        assert not fake_connection.in_transaction
 
-def test_can_commit_transaction(connection_facade: ConnectionFacade, fake_connection: FakeConnection) -> None:
-    connection_facade.commit()
-    assert fake_connection.committed
-
-
-def test_can_rollback_transaction(connection_facade: ConnectionFacade, fake_connection: FakeConnection) -> None:
-    connection_facade.start()
-    connection_facade.rollback()
-    assert not fake_connection.in_transaction
-
-
-def test_can_close_connection(connection_facade: ConnectionFacade, fake_connection: FakeConnection) -> None:
-    connection_facade.close()
-    assert not fake_connection.is_connected
+    @staticmethod
+    def test_can_close_connection(connection_facade: ConnectionFacade, fake_connection: FakeConnection) -> None:
+        connection_facade.close()
+        assert not fake_connection.is_connected
