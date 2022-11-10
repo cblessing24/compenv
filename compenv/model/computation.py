b73a8d5313126c0ab88408d16fed4d8dd05d8ed8
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-11-10 12:47:39 +0000

    Add recording service

--- compenv/model/computation.py
+++ compenv/model/computation.py
@@ -1,9 +1,10 @@
 """Contains model objects related to computation."""
 from __future__ import annotations
 
+from abc import ABC, abstractmethod
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Iterable, Mapping, NewType, Optional
+from typing import Callable, Iterable, Mapping, NewType, Optional
 
 from .environment import Environment
 
@@ -39,3 +40,41 @@ class Algorithm:
     def __getitem__(self, environment: Environment) -> frozenset[Computation]:
         """Get all computations of the algorithm in the given environment."""
         return frozenset(self._computations[environment])
+
+
+class AlgorithmRepository(ABC):
+    """A repository containing algorithms."""
+
+    @abstractmethod
+    def add(self, algorithm: Algorithm) -> None:
+        """Add an algorithm to the repository."""
+
+    @abstractmethod
+    def get(self, name: AlgorithmName) -> Algorithm:
+        """Get the algorithm with the given name."""
+
+
+class EnvironmentDeterminingService(ABC):  # pylint: disable=too-few-public-methods
+    """A service that is able to determine the environment."""
+
+    @abstractmethod
+    def determine(self) -> Environment:
+        """Determine the environment."""
+
+
+class RecordingService:  # pylint: disable=too-few-public-methods
+    """A service that is able to record the environment during a computation."""
+
+    def __init__(
+        self, algorithm_repository: AlgorithmRepository, environment_determining_service: EnvironmentDeterminingService
+    ) -> None:
+        """Initialize the recording service."""
+        self.algorithm_repository = algorithm_repository
+        self.environment_determining_service = environment_determining_service
+
+    def record(self, algorithm_name: AlgorithmName, arguments: Arguments, trigger: Callable[[Arguments], None]) -> None:
+        """Record the environment during the execution of the algorithm on the given arguments."""
+        trigger(arguments)
+        algorithm = self.algorithm_repository.get(algorithm_name)
+        environment = self.environment_determining_service.determine()
+        algorithm.execute(environment, arguments)
--- tests/unit/model/test_computation.py
+++ tests/unit/model/test_computation.py
@@ -1,4 +1,16 @@
-from compenv.model.computation import Algorithm, AlgorithmName, Arguments, Computation
+from __future__ import annotations
+
+from typing import Optional
+
+from compenv.model.computation import (
+    Algorithm,
+    AlgorithmName,
+    AlgorithmRepository,
+    Arguments,
+    Computation,
+    EnvironmentDeterminingService,
+    RecordingService,
+)
 from compenv.model.environment import Environment
 
 
@@ -23,3 +35,47 @@ def test_can_instantiate_with_computations() -> None:
         computations={environment: [computation]},
     )
     assert algorithm[environment] == frozenset([computation])
+
+
+def test_can_record_computation() -> None:
+    class FakeAlgorithmRepository(AlgorithmRepository):
+        def __init__(self) -> None:
+            self.algorithms: dict[AlgorithmName, Algorithm] = {}
+
+        def add(self, algorithm: Algorithm) -> None:
+            self.algorithms[algorithm.name] = algorithm
+
+        def get(self, name: AlgorithmName) -> Algorithm:
+            return self.algorithms[name]
+
+    class FakeComputationTrigger:
+        def __init__(self) -> None:
+            self.arguments: Optional[Arguments] = None
+
+        def __call__(self, arguments: Arguments) -> None:
+            self.arguments = arguments
+
+    class FakeEnvironmentDeterminingService(EnvironmentDeterminingService):
+        def __init__(self, environment: Environment) -> None:
+            self.environment = environment
+
+        def determine(self) -> Environment:
+            return self.environment
+
+    algorithm_name = AlgorithmName("myalgorithm")
+    repository = FakeAlgorithmRepository()
+    repository.add(Algorithm(algorithm_name))
+    trigger = FakeComputationTrigger()
+    arguments = Arguments("myarguments")
+    environment = Environment(frozenset())
+    environment_determining_service = FakeEnvironmentDeterminingService(environment)
+    recording_service = RecordingService(
+        algorithm_repository=repository, environment_determining_service=environment_determining_service
+    )
+
+    recording_service.record(algorithm_name=algorithm_name, arguments=arguments, trigger=trigger)
+
+    assert trigger.arguments == arguments
+    assert repository.get(algorithm_name)[environment] == frozenset(
+        [Computation(algorithm_name, arguments, environment)]
+    )
