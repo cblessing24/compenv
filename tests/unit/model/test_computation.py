5cdbedf3d5da44911e1c6820e55880238665c652
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-11-11 13:15:05 +0000

    Add create_registry fixture

--- tests/unit/model/test_computation.py
+++ tests/unit/model/test_computation.py
@@ -104,40 +104,39 @@ def create_environment() -> EnvironmentCreator:
     return create
 
 
+class RegistryCreator(Protocol):
+    def __call__(self, algorithm_name: str, computations: Optional[Iterable[Computation]] = ...) -> ComputationRegistry:
+        ...
+
+
+@pytest.fixture
+def create_registry() -> RegistryCreator:
+    def create(algorithm_name: str, computations: Optional[Iterable[Computation]] = None) -> ComputationRegistry:
+        return ComputationRegistry(AlgorithmName(algorithm_name), computations)
+
+    return create
+
+
 def test_can_add_computation_to_registry(
-    create_computation: ComputationCreator, create_environment: EnvironmentCreator
+    create_registry: RegistryCreator, create_computation: ComputationCreator, create_environment: EnvironmentCreator
 ) -> None:
-    registry = ComputationRegistry(AlgorithmName("myalgorithm"))
+    registry = create_registry("myalgorithm")
     computation = create_computation("myalgorithm", "myarguments", create_environment())
     registry.add(computation)
     assert registry.get(computation.specification) == computation
 
 
-def test_can_not_add_computation_produced_by_different_algorithm_to_registry(
-    create_computation: ComputationCreator, create_environment: EnvironmentCreator
+def test_can_not_add_computation_produced_by_different_algorithms_to_registry(
+    create_registry: RegistryCreator, create_computation: ComputationCreator, create_environment: EnvironmentCreator
 ) -> None:
-    registry = ComputationRegistry(AlgorithmName("myalgorithm"))
     with pytest.raises(ValueError, match="Expected '.*' for algorithm name of computation, got '.*'"):
-        registry.add(create_computation("myotheralgorithm", "myarguments", create_environment()))
+        create_registry("myalgorithm").add(create_computation("myotheralgorithm", "myarguments", create_environment()))
 
 
-def test_can_not_add_computations_with_identical_specifications(create_environment: EnvironmentCreator) -> None:
-    specification = Specification(AlgorithmName("myalgorithm"), Arguments("myarguments"))
-    registry = ComputationRegistry(AlgorithmName("myalgorithm"))
-    computation1 = Computation(specification, create_environment())
-    registry.add(computation1)
-    computation2 = Computation(specification, create_environment([("mydistribution", "0.1.1")]))
-    with pytest.raises(ValueError, match="Duplicate specification:"):
-        registry.add(computation2)
-
-
-def test_can_instantiate_registry_with_computations(
-    create_computation: ComputationCreator, create_environment: EnvironmentCreator
+def test_can_not_add_computations_with_identical_specifications(
+    create_registry: RegistryCreator, create_environment: EnvironmentCreator
 ) -> None:
-    environment = create_environment()
-    computation = create_computation("myalgorithm", "myarguments", environment)
-    registry = ComputationRegistry(
-        AlgorithmName("myalgorithm"),
-        computations={computation},
-    )
-    assert set(registry.list(environment)) == {computation}
+    specification = Specification(AlgorithmName("myalgorithm"), Arguments("myarguments"))
+    registry = create_registry("myalgorithm", [Computation(specification, create_environment())])
+    with pytest.raises(ValueError, match="Duplicate specification:"):
+        registry.add(Computation(specification, create_environment([("mydistribution", "0.1.1")])))
